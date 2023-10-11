62e27ba3b26de79d7b9a01e116c12b94c37de468
Haider Hirkani <haider.hirkani21@vit.edu>
2023-10-11 18:19:27 +0530

    Evaluation Info Extract Done

--- extraction_script.py
+++ extraction_script.py
@@ -47,3 +47,37 @@ def review_guide_extract(review_info):
     if communication_section is not None:
         return communication_section.group(0)
 
+
+def evaluation_info_extract(evaluation_info):
+    # Overall Score
+    score_pattern = "Your Score .*"
+    overall_score = re.search(score_pattern, evaluation_info).group(0)[-6:].strip()
+    # Overall Summary
+    summary_pattern = r"Summary[\s\S]+Per Competency Score?"
+    summary = re.search(summary_pattern, evaluation_info).group(0).strip()
+    # Communication Score
+    communication_pattern = r"Communication .*"
+    communication_search = re.search(communication_pattern, evaluation_info)
+    communication_score = communication_search.group(0).strip()
+    # Tips to Improve
+    tips_pattern = r"(?i)(Tips to Improve)"
+    tips_search = re.search(tips_pattern, evaluation_info)
+    errors_search = None
+    communication_summary = ""
+    errors_info = ""
+    tips_to_improve = ""
+    if tips_search is not None:
+        tips_to_improve = evaluation_info[tips_search.start():].strip()
+    else:
+        # Errors Info
+        errors_pattern = r"(?:Spelling|Grammar)"
+        errors_search = re.search(errors_pattern, evaluation_info)
+    # Communication Summary
+    if communication_search is not None:
+        start = communication_search.end()
+        if tips_search is not None:
+            communication_summary = evaluation_info[start: tips_search.start()].strip()
+        elif errors_search is not None:
+            communication_summary = evaluation_info[start: errors_search.start()].strip()
+            errors_info = evaluation_info[errors_search.start():].strip()
+    return overall_score, summary, communication_score, communication_summary, errors_info, tips_to_improve
