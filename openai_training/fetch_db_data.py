c64d0b9fd26e4fbacf88c4482211f1efee4405fb
Haider Hirkani <haider.hirkani21@vit.edu>
2023-10-16 16:20:55 +0530

    Added the training files scripts and JSONL files along with other scripts for fetching data from DB

---
+++ openai_training/fetch_db_data.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from data_analysis.schema import CaseStudy, CaseStudyEvaluation
+
+
+def fetch_case_study(cs_id: int, session: Session) -> tuple:
+    record = session.get(CaseStudy, cs_id)
+    if record is not None:
+        instructions = record.instructions
+        abbreviations = record.abbreviations
+        email_instructions = record.email_instructions
+        context = record.context
+        name = record.name
+        return name, instructions, abbreviations, email_instructions, context
+
+
+def fetch_case_study_evaluation(cs_id: int, session: Session) -> list:
+    all_records = session.query(CaseStudyEvaluation).filter(CaseStudyEvaluation.case_study_id == cs_id).all()
+    records_list = []
+    for record in all_records:
+        overall_score = record.overall_score
+        overall_summary = record.overall_summary
+        communication_score = record.communication_score
+        communication_summary = record.communication_summary
+        communication_errors = record.communication_errors
+        communication_tips = record.communication_tips
+        trainee_answer = record.trainee_answer
+        records_list.append(
+            [overall_score, overall_summary, communication_score, communication_summary, communication_errors,
+             communication_tips, trainee_answer])
+    return records_list
---
+++ openai_training/make_training_data.py
@@ -0,0 +1,59 @@
+import json
+from config import settings
+
+
+def create_evaluation_sample(name: str, instructions: str, abbreviations: str,
+                             email_instructions: str, context: str, trainee_ans: str) -> str:
+    sample_data = f"Case Study Name: \n{name}\n\n" \
+                  f"Instructions/Important Notice: \n{instructions}\n\n" \
+                  f"Abbreviations (if any): \n{abbreviations}\n\n" \
+                  f"Email Instructions to be followed: \n{email_instructions}\n\n" \
+                  f"Case Study Content: \n{context}\n\n" \
+                  f"Trainee's Answer: \n{trainee_ans}\n"
+    return sample_data
+
+
+def create_overall_content(overall_summary: str, overall_score: float) -> str:
+    data = f"Overall Score (out-off 10): {overall_score}\n\n" \
+           f"Overall Summary: \n{overall_summary}\n"
+    return data
+
+
+def create_communication_content(communication_summary: str, communication_score: float) -> str:
+    data = f"Communication Score(out-off 10): {communication_score}\n\n" \
+           f"Communication Summary: \n{communication_summary}\n"
+    return data
+
+
+def create_tips_errors(tips: str, errors: str) -> str:
+    data = f"Tips/Suggestions for Improvement (if any): \n{tips}\n\n" \
+           f"Spelling/Grammar Errors (if any): \n{errors}\n"
+    return data
+
+
+def create_tips_errors_content(user_content: str, tips_errors_content: str) -> dict:
+    return {"messages": [{"role": "system", "content": settings.TIPS_ERRORS_MESSAGE},
+                         {"role": "user", "content": user_content},
+                         {"role": "assistant", "content": tips_errors_content}
+                         ]}
+
+
+def create_overall_score(user_content: str, overall_content: str) -> dict:
+    return {"messages": [{"role": "system", "content": settings.OVERALL_SCORE_SUMMARY_MESSAGE},
+                         {"role": "user", "content": user_content},
+                         {"role": "assistant", "content": overall_content}
+                         ]}
+
+
+def create_communication_score(user_content: str, communication_content: str) -> dict:
+    return {"messages": [{"role": "system", "content": settings.COMMUNICATION_SCORE_SUMMARY_MESSAGE},
+                         {"role": "user", "content": user_content},
+                         {"role": "assistant", "content": communication_content}
+                         ]}
+
+
+def create_jsonl_file(data: list, filename: str) -> None:
+    with open(filename, 'w') as jsonl_file:
+        for record in data:
+            json_record = json.dumps(record)
+            jsonl_file.write(json_record + '\n')
